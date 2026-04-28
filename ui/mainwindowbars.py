897d0ab784e7bd922283df8caf6399a008f20783
Thomas Lynch <thomaswantstobeaskeleton@gmail.com>
2026-04-28 15:01:13 -0700

    Fix ambiguous Go shortcuts by removing duplicate QAction bindings

--- ui/mainwindowbars.py
+++ ui/mainwindowbars.py
@@ -599,13 +599,9 @@ class TitleBar(Widget):
         self.goToolBtn = TitleBarToolBtn(self)
         self.goToolBtn.setText(self.tr('Go'))
         prevPageAction = QAction(self.tr('Previous Page'), self)
-        prevPageAction.setShortcut(QKeySequence.fromString(get_shortcut("go.prev_page", getattr(pcfg, "shortcuts", None))))
         nextPageAction = QAction(self.tr('Next Page'), self)
-        nextPageAction.setShortcut(QKeySequence.fromString(get_shortcut("go.next_page", getattr(pcfg, "shortcuts", None))))
         prevPageAltAction = QAction(self.tr('Previous Page (alternate)'), self)
-        prevPageAltAction.setShortcut(QKeySequence.fromString(get_shortcut("go.prev_page_alt", getattr(pcfg, "shortcuts", None))))
         nextPageAltAction = QAction(self.tr('Next Page (alternate)'), self)
-        nextPageAltAction.setShortcut(QKeySequence.fromString(get_shortcut("go.next_page_alt", getattr(pcfg, "shortcuts", None))))
         goMenu = QMenu(self.goToolBtn)
         goMenu.addActions([prevPageAction, nextPageAction])
         goMenu.addSeparator()
@@ -616,12 +612,6 @@ class TitleBar(Widget):
         self.nextpage_trigger = nextPageAction.triggered
         prevPageAltAction.triggered.connect(self.prevpage_trigger)
         nextPageAltAction.triggered.connect(self.nextpage_trigger)
-        self._shortcut_actions_title.extend([
-            ("go.prev_page", "PageUp", prevPageAction),
-            ("go.next_page", "PageDown", nextPageAction),
-            ("go.prev_page_alt", "A", prevPageAltAction),
-            ("go.next_page_alt", "D", nextPageAltAction),
-        ])
 
         # Tools menu
         self.toolsToolBtn = TitleBarToolBtn(self)
